857947070c8d12834b8413e8c80fd283002cf06b
vvdung <vvdung@hueuni.edu.vn>
2026-01-22 00:12:38 +0700

    devkitC-v4

---
+++ TIN4024/1_LED_Blink_DevkitC_v4/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  printf("Hello, World!\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  printf("Loop running ....\n");
+  delay(1000);
+}
+
